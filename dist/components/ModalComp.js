a4b207e4e0f68b6e0f11bf0b2e7ee28e23a4dcae
Morgan <morgand@tutamail.com>
2023-02-16 09:20:42 +0100

    add build

--- dist/components/ModalComp.js
+++ dist/components/ModalComp.js
@@ -3,14 +3,18 @@ import "./ModalComp.css";
 export default function ModalComp({
   onClose,
   modalContent,
-  modalStyle
+  modalStyle,
+  buttonStyle,
+  containerStyle
 }) {
   return /*#__PURE__*/React.createElement("div", {
+    style: containerStyle,
     className: "modalComp"
   }, /*#__PURE__*/React.createElement("div", {
     style: modalStyle,
     className: "modalComp__content"
   }, /*#__PURE__*/React.createElement("p", null, modalContent ? modalContent : "Modal content"), /*#__PURE__*/React.createElement("button", {
+    style: buttonStyle,
     className: "modalComp__button",
     onClick: () => onClose()
   }, "X")));
